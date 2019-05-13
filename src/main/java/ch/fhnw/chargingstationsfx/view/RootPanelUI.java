bd128c57d41c71e889b771dc15018be53f820345
Joyce Hänggi <joyce.haenggi@students.fhnw.ch>
2019-05-13 14:58:06 +0200

    added Overview

--- src/main/java/ch/fhnw/chargingstationsfx/view/RootPanelUI.java
+++ src/main/java/ch/fhnw/chargingstationsfx/view/RootPanelUI.java
@@ -1,21 +1,18 @@
 package ch.fhnw.chargingstationsfx.view;
 
-import ch.fhnw.chargingstationsfx.presentationmodel.Ladestation;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
 
 import ch.fhnw.chargingstationsfx.presentationmodel.RootPM;
 
 public class RootPanelUI extends BorderPane implements ViewMixin {
     private final RootPM rootPM;
-    private TableView<Ladestation> tabelle;
-    private Object GridPaneStarter;
+    private SplitPane splitPane = new SplitPane();
+    private SplitPane splitPane1 = new SplitPane();
 
 
     public RootPanelUI(RootPM model) {
         this.rootPM = model;
-
         init();
     }
 
@@ -26,37 +23,16 @@ public class RootPanelUI extends BorderPane implements ViewMixin {
 
     @Override
     public void initializeControls() {
-
-
-        tabelle = new TableView<>(rootPM.getResultate());
-
-        //Im Konstruktor wird die Überschrift der Spalte angegeben
-        TableColumn<Ladestation, String> strasseCol = new TableColumn<>("Strasse");
-        strasseCol.setCellValueFactory(cel -> cel.getValue().strasseNameProperty());
-
-        TableColumn<Ladestation, Number> PLZCol = new TableColumn<>("PLZ");
-        PLZCol.setCellValueFactory(cel -> cel.getValue().PLZProperty());
-
-        TableColumn<Ladestation, String> ortCole = new TableColumn<>("Ort");
-        ortCole.setCellValueFactory(cel -> cel.getValue().ortProperty());
-
-        TableColumn<Ladestation, Number> anzahlLadestationCol = new TableColumn<>("Anzahl Ladestationen");
-        anzahlLadestationCol.setCellValueFactory(cel -> cel.getValue().numberOfChargingPointsProperty());
-
-        tabelle.getColumns().addAll(strasseCol, PLZCol, ortCole, anzahlLadestationCol);
-
-
     }
 
     @Override
     public void layoutControls() {
-        //setVgrow(tabelle, Priority.ALWAYS);
-
-        //getChildren().addAll(tabelle);
-        setTop(new LayoutTop());
-        setLeft(tabelle);
-        setRight(new LayoutRight());
-        setCenter(new LayoutCenter());
+        setTop(new Header());
+        splitPane.getItems().addAll(new Overview(rootPM));
+        setLeft(splitPane);
+        splitPane1.getItems().add(new Editor());
+        setRight(splitPane1);
+        //setCenter(new Center());
 
 
     }
